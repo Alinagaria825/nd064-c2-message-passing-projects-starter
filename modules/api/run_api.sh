06a7adc76073da5c478d9910d8479f846519b688
alinagaria825 <anagaria182@gmail.com>
2020-11-30 17:43:01 +0100

    modified files

---
+++ modules/api/run_api.sh
@@ -0,0 +1,5 @@
+docker build -t alinagaria825/udaconnect-api .
+
+docker run -it --name api alinagaria825/udaconnect-api
+
+docker push alinagaria825/udaconnect-api:latest
---
+++ modules/frontend/run_frontend.sh
@@ -0,0 +1,5 @@
+docker build -t alinagaria825/udaconnect-app .
+
+docker run -it --name frontend alinagaria825/udaconnect-app
+
+docker push alinagaria825/udaconnect-app:latest
